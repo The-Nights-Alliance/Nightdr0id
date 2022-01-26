b172c3f590f44466aa245f32851810f832c57920
maverickquill55 <76607214+maverick-dev-55@users.noreply.github.com>
2022-01-26 18:34:28 +0000

    fix dumb errors

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,18 +2,19 @@ module.exports = (client, message) => {
     if (message.author.bot) return;
     if (message.webhookId) return;
     if (message.content.indexOf(client.config.prefix) !== 0) {
-        client.db.points.ensure(`${message.guild.id}-${message.author.id}`, {
+       const key = `${message.guild.id}-${message.author.id}`
+         client.db.points.ensure(key, {
             user: message.author.id,
             guild: message.guild.id,
             points: 0,
             level: 1
         });
-        client.points.inc(key, "points");
+        client.db.points.inc(key, "points", 500);
         const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, "points")));
         const channel = message.guild.channels.cache.find(c => c.name === "levelups")
-        if (client.points.get(key, "level") < curLevel) {
+        if (client.db.points.get(key, "level") < curLevel) {
             channel.send(`${message.author.username} leveled up to level **${curLevel}**!`);
-            client.points.set(key, curLevel, "level");
+            client.db.points.set(key, curLevel, "level");
         }
 
         if(client.chanevents.get(message.channel.name)) {
